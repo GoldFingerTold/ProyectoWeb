7ba7e7435819f7fa385e3092bb83ff983f41407a
Hugo Ceriani <100979006+GoldFingerTold@users.noreply.github.com>
2023-07-01 19:34:29 -0300

    se arreglo el carro

--- proyectoWebApp/views.py
+++ proyectoWebApp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, HttpResponse
-
+from carro.carro import Carro
 
 # Create your views here.
 
 def home(request):
+
+    carro=Carro(request)
 
     return render(request, "proyectoWebAPP/home.html")
 
