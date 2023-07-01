24191fac0a38b16aea759954fb8b4a326ff76e62
Hugo Ceriani <100979006+GoldFingerTold@users.noreply.github.com>
2023-07-01 14:15:17 -0300

    se agrego login

--- autenticacion/urls.py
+++ autenticacion/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import VRegistro
+from .views import VRegistro, cerrar_sesion, logear
 
 
 
@@ -8,4 +8,6 @@ from .views import VRegistro
 urlpatterns = [
    
      path('',VRegistro.as_view(), name="Autenticacion"),
+     path('cerrar_sesion',cerrar_sesion, name="cerrar_sesion"),
+     path('logear',logear, name="logear"),
 ]
--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 
